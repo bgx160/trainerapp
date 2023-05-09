c692522327c4c640900fead910471a83598c5594
Topias Mantela <104072848+thoby2@users.noreply.github.com>
2023-05-09 18:06:44 +0300

    add useCallback

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,20 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 
 function useFetch(url) {
     const [data, setData] = useState([]);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
 
-    useEffect(() => fetchData, [url]);
-
-    const fetchData = () => {
+    const fetchData = useCallback(() => {
         setLoading(true);
         fetch(url)
             .then(res => res.json())
             .then(data => setData(data))
             .catch(e => setError(e))
             .finally(setLoading(false));
-    }
+    }, [url])
+
+    useEffect(() => fetchData, [url, fetchData]);
+
 
     return { data, error, loading, fetchData }
 }
